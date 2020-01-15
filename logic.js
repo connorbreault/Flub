4ff755f5dc3cb9c0a34db6e6204b2f6316cd17a5
Spaghetti Jones <connor.breault@comcast.net>
2020-01-15 01:25:35 -0800

    js animation trigger

--- logic.js
+++ logic.js
@@ -3,6 +3,18 @@ $(document).ready(function () {
     $('.materialboxed').materialbox();
 });
 
+function js_Load() {
+    document.body.style.visibility = 'visible'
+    transitions()
+}
+
+function transitions() {
+    $("#definition").addClass("definitionAnimation")
+    $("#headLogo").addClass("headLogoAnimation")
+    $(".mainCollapsible").addClass("mainCollapsibleAnimation")
+
+}
+
 $("#FCSignupButton").on("click", function () {
     let newSignup = $("#signupEmail").val()
     if (newSignup === "") {
