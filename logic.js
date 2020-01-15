0c40c41fac95b871365f6787bd27eabff9653e7e
Spaghetti Jones <connor.breault@comcast.net>
2020-01-14 19:33:43 -0800

    contact and flub club bodies filled

--- logic.js
+++ logic.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
     $('.collapsible').collapsible();
 });
+$("#FCSignupButton").on("click", function () {
+    let newSignup = $("#signupEmail").val()
+    if (newSignup === "") {
+        alert("Please fill out the input")
+    } else {
+        alert(newSignup)
+    }
+})
