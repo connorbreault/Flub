eac1adb562230800ba62aa1b26bbddd964ac57bc
Spaghetti Jones <connor.breault@comcast.net>
2020-01-15 18:12:05 -0800

    flubclub firebase link

--- logic.js
+++ logic.js
@@ -1,18 +1,38 @@
-$(document).ready(function () {
-    $('.collapsible').collapsible();
-    $('.materialboxed').materialbox();
-});
-
 function js_Load() {
     document.body.style.visibility = 'visible'
 }
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+    $('.materialboxed').materialbox();
 
-$("#FCSignupButton").on("click", function () {
-    let newSignup = $("#signupEmail").val()
-    if (newSignup === "") {
-        alert("Please fill out the input")
-    } else {
-        $("#FCSignup").addClass("hide")
-        $("#thanksSignup").removeClass("hide")
-    }
-})
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyBqb9rA1e9tT3uCObNH25CzqPXGGulVbGM",
+        authDomain: "flubclub-rubhub.firebaseapp.com",
+        databaseURL: "https://flubclub-rubhub.firebaseio.com",
+        projectId: "flubclub-rubhub",
+        storageBucket: "flubclub-rubhub.appspot.com",
+        messagingSenderId: "563265812210",
+        appId: "1:563265812210:web:1434b64f9dc816096f026b",
+        measurementId: "G-GW6DJGRPD4"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    var database = firebase.database()
+    let newSignup = ""
+    $("#FCSignupButton").on("click", function () {
+        let newSignup = $("#signupEmail").val()
+        if (newSignup === "") {
+            alert("Please fill out the input")
+        } else {
+            $("#signedupEmail").html(newSignup)
+            firebase.database().ref().push({
+                email: newSignup,
+                // dateAdded: firebase.database.ServerValue
+            })
+            $("#FCSignup").addClass("hide")
+            $("#thanksSignup").removeClass("hide")
+        }
+    })
+});
