c3aea8146321b7ae8e620141de203daa6323aa2d
Spaghetti Jones <connor.breault@comcast.net>
2020-01-15 22:32:37 -0800

    tour date section filled out

--- logic.js
+++ logic.js
@@ -23,6 +23,84 @@ $(document).ready(function () {
     firebase.initializeApp(firebaseConfig);
     var database = firebase.database()
 
+
+    // Render Shows
+    const shows = [
+        {
+            date: "1-24",
+            city: "Orangeville, Ca",
+            venue: "The Boardwalk",
+            time: "TBD",
+            bands: ["The Kennedy Veil", "Flub", "Equipoise"],
+            link: ""
+        },
+        {
+            date: "1-25",
+            city: "Lakewood, Ca",
+            venue: "Regal Inn",
+            time: "TBD",
+            bands: ["The Kennedy Veil", "Flub", "Equipoise"],
+            link: ""
+        },
+        {
+            date: "1-26",
+            city: "Santa Rosa, Ca",
+            venue: "The Arlene Francis Center",
+            time: "TBD",
+            bands: ["The Kennedy Veil", "Flub", "Equipoise"],
+            link: ""
+        },
+    ]
+    for (let i = 0; i < shows.length; i++) {
+        let showListItem = (`
+        <li>
+            <div class="collapsible-header"
+                style="background: none; border: gold solid 1px; border-radius: 15px">
+                <div style="width: 25%">
+                    ${shows[i].date}
+                </div>
+                <div style="width: 75%">
+                    ${shows[i].city}
+                </div>
+            </div>
+            <div class="collapsible-body" style="border: none; padding: 0px">
+                <div class="row">
+                    <div class="col s8">
+                        <h6>Venue</h6>
+                        <p style="word-wrap: break-word">
+                        ${shows[i].venue}
+                        </p>
+                    </div>
+                    <div class="col s4">
+                        <h6>Time</h6>
+                        <p>
+                        ${shows[i].time}
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s12">
+                        <h6>Bands</h6>
+                        <ol>
+                        ${shows[i].bands.map(function (band) {
+            return "<li>" + band + "</li>"
+        })}
+                        </ol>
+                        </div>
+                        </div>
+                        <div class="row">
+                        <div class="col s12">
+                        <a class="waves-effect waves-light btn buyTickets" href="${shows[i].time}">Buy
+                        Tickets</a>
+                        </div>
+                        </div>
+                        </div>
+                        </li>
+                        `)
+        $("#showCollapsible").append(showListItem)
+    }
+
+
     // Empty signup
     let newSignup = ""
 
